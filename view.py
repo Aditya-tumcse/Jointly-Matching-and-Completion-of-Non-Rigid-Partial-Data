807e9488cf7b5e41dbd85b5530de89a693b2794b
ge46tik <aditya.srinivas@tum.de>
2021-01-20 22:12:45 +0100

    m3dlookat function returns the camera extrinsic matrix in the column major form

--- view.py
+++ view.py
@@ -1,28 +1,16 @@
 import math
 import numpy as np
 
-def length(v):
-    return math.sqrt(v[0]*v[0]+v[1]*v[1]+v[2]*v[2])
-
 def normalize(v):
-    l = length(v)
+    l = np.linalg.norm(v)
     return [v[0]/l, v[1]/l, v[2]/l]
 
-def dot(v0, v1):
-    return v0[0]*v1[0]+v0[1]*v1[1]+v0[2]*v1[2]
-
-def cross(v0, v1):
-    return [
-        v0[1]*v1[2]-v1[1]*v0[2],
-        v0[2]*v1[0]-v1[2]*v0[0],
-        v0[0]*v1[1]-v1[0]*v0[1]]
-
 def m3dLookAt(eye, target, up):
-    mz = normalize( (eye[0]-target[0], eye[1]-target[1], eye[2]-target[2]) ) # inverse line of sight
-    mx = normalize( cross( up, mz ) )
-    my = normalize( cross( mz, mx ) )
+    mz = normalize(eye - target)
+    mx = normalize(np.cross(up,mz))
+    my = normalize(np.cross(mz,mx))
     tx =  eye[0]
     ty =  eye[1]
-    tz = -eye[2]   
+    tz = eye[2]   
     
-    return np.array([mx[0], my[0], mz[0], 0, mx[1], my[1], mz[1], 0, mx[2], my[2], mz[2], 0, tx, ty, tz, 1]).reshape(4,4)
+    return np.array([[mx[0], my[0], mz[0], tx],[mx[1], my[1], mz[1], ty],[mx[2], my[2], mz[2], tz],[0, 0, 0, 1]])
