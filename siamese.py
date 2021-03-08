f0a49dcbb5357ffd09082b2f434cd66924f2ed24
ge46tik <aditya.srinivas@tum.de>
2021-03-08 17:40:17 +0100

    Initial commit of siamese network architecture

---
+++ siamese.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Siamese(nn.Module):
+    def __init__(self,model):
+        super(Siamese,self).__init__()
+        self.model = model
+        self.fc = nn.Linear(512,2)
+
+    def forward_once(self,x):
+        out = self.moel(x)
+        out = F.relu(self.fc(out))
+        return out
+
+    def forward(self,input_1,input_2):
+        out_1 = self.forward_once(input_1)
+        out_2 = self.forward_once(input_2)
+        return(out_1,out_2)
