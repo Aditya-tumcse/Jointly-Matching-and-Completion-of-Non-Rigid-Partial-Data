8495f1d1ce4d884426a0b9151fccc8f96cb00eb7
ge46tik <aditya.srinivas@tum.de>
2021-03-10 17:50:23 +0100

    Initial commit defining hyperparameters

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config():
+    training_set_dir = "..."
+    validation_set_dir = "..."
+    train_batch_size = 64
+    train_number_epochs = 100
+    learning_rate = 0.0001
+    contrastive_margin = 1.0
+    resnet_depth = 18
+    
