cf3f8d188b0c412797e39a8fba526c566f36d1c4
ge46tik <aditya.srinivas@tum.de>
2021-03-12 12:45:30 +0100

    Added a new class to provide parameters for training data

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 class Config():
-    training_set_dir = "..."
-    validation_set_dir = "..."
+    training_set_dir = "/cluster/sorona/hyu/ptdcv/ballet/"
+    validation_set_dir = "/cluster/sorona/hyu/ptdcv/goalkeeper"
     train_batch_size = 64
     train_number_epochs = 100
     learning_rate = 0.0001
     contrastive_margin = 1.0
     resnet_depth = 18
+
+class Training_Data_Config():
+    number_keypoints = 400
+    stride = 4
     
