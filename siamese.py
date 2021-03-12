73028d953aabc1531100172715eb23c52d562cf4
ge46tik <aditya.srinivas@tum.de>
2021-03-12 12:47:12 +0100

    Changes to the network architecture

--- siamese.py
+++ siamese.py
@@ -1,23 +1,20 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 
 class Siamese(nn.Module):
     def __init__(self,model):
         super(Siamese,self).__init__()
         self.model = model
-        self.fc = nn.Linear(512,2)
 
-    def forward_once(self,x):
-        out = self.model(x)
-        out = F.relu(self.fc(out))
-        return out
 
     def forward(self,input_1,input_2):
-        out_1 = self.forward_once(input_1)
-        out_2 = self.forward_once(input_2)
+        out_1 = self.model(input_1)
+        out_2 = self.model(input_2)
         return(out_1,out_2)
+
+    def _get_model_(self,x):
+        return(self.model(x))
 
 """
 #for test.Import 3D ResNet architecture to test
