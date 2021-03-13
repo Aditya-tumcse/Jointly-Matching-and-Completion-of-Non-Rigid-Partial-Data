e1476de12d33d0e0f953d54a10da17f116810fba
ge46tik <aditya.srinivas@tum.de>
2021-03-13 12:33:21 +0100

    Added validation params

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import torch
 
 class Config():
     training_set_dir = "/home/aditya/PycharmProjects/OpenCV-python/Project_2/patch_files/"
-    validation_set_dir = "/cluster/sorona/hyu/ptdcv/goalkeeper"
     train_batch_size = 64
     train_number_epochs = 100
     learning_rate = 0.0001
@@ -15,4 +14,10 @@ class Config():
 class Training_Data_Config():
     number_keypoints = 400
     stride = 4
-    training_data_size = 2
+    training_data_size = 50000
+
+class Validation_Data_Config():
+    number_keypoints = 416
+    stride = 4
+    validation_data_size = 20000
+    validation_set_dir = "/cluster/sorona/hyu/ptdcv/goalkeeper/"
