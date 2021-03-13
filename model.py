21624afe228ef0c22edf8491aeb334f1483af04c
ge46tik <aditya.srinivas@tum.de>
2021-03-13 19:47:38 +0100

    Changed the name of config file

--- model.py
+++ model.py
@@ -1,8 +1,8 @@
 import train
 import wandb
-import config
+import configuration
 
-params = dict(epochs=config.Config.train_number_epochs,batch_size=config.Config.train_batch_size,lr=config.Config.learning_rate,dataset_size=config.Training_Data_Config.training_data_size,loss_margin=config.Config.contrastive_margin,resnet_depth=config.Config.resnet_depth)
+params = dict(epochs=configuration.Config.train_number_epochs,batch_size=configuration.Config.train_batch_size,lr=configuration.Config.learning_rate,dataset_size=configuration.Training_Data_Config.training_data_size,loss_margin=configuration.Config.contrastive_margin,resnet_depth=configuration.Config.resnet_depth)
 
 if __name__ == '__main__':
     wandb.login()
