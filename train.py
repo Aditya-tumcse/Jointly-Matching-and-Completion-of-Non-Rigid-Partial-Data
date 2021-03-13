787874fe3ce720ab12716a0e908eebb643064510
ge46tik <aditya.srinivas@tum.de>
2021-03-13 19:48:01 +0100

    Changed the name of config file

--- train.py
+++ train.py
@@ -13,19 +13,19 @@ import wandb
 import resnet
 import siamese
 import loss
-import config
+import configuration
 
 def wandb_initiliazer(arguments):
     with wandb.init(project="TDCV_2", config=arguments):
-        config1 = wandb.config
+        config = wandb.config
 
-        model, train_loader,val_loader,loss_fn, optimizer = nn_model(config1)
+        model, train_loader,val_loader,loss_fn, optimizer = nn_model(config)
 
-        train(model, train_loader, val_loader, loss_fn, optimizer, config)
+        train(model, train_loader, val_loader, loss_fn, optimizer, configuration)
     return model
 
 
-def nn_model(config1):
+def nn_model(config):
     data_transforms = transforms.Compose([transforms.ToTensor()]) 
 
     
@@ -33,20 +33,20 @@ def nn_model(config1):
     validation_set = GoalKeeper(data_transforms)
     
     #Loading train and validation set
-    train_set_loader = DataLoader(train_set,batch_size=config.Config.train_batch_size,shuffle=False,num_workers=config.Config.number_workers)
-    validation_set_loader = DataLoader(validation_set,batch_size=config.Config.train_batch_size,shuffle=false,num_workers=config.Config.number_workers)
+    train_set_loader = DataLoader(train_set,batch_size=configuration.Config.train_batch_size,shuffle=False,num_workers=configuration.Config.number_workers)
+    validation_set_loader = DataLoader(validation_set,batch_size=configuration.Config.train_batch_size,shuffle=False,num_workers=configuration.Config.number_workers)
     
     #Build the model
-    intermediate_net = resnet.generate_model(config.Config.resnet_depth)
+    intermediate_net = resnet.generate_model(configuration.Config.resnet_depth)
     siamese_net = siamese.Siamese(intermediate_net)
 
-    loss_function = loss.ContrastiveLoss(config.Config.contrastive_margin)
+    loss_function = loss.ContrastiveLoss(configuration.Config.contrastive_margin)
 
-    if config.Config.device.type == 'cuda':
+    if configuration.Config.device.type == 'cuda':
         siamese_net.cuda()
 
     
-    optimizer = torch.optim.Adam(siamese_net.parameters(),lr=config.Config.learning_rate)
+    optimizer = torch.optim.Adam(siamese_net.parameters(),lr=configuration.Config.learning_rate)
     return siamese_net,train_set_loader,validation_set_loader,loss_function,optimizer
 
 def validation_phase(NN_model,val_set_loader,loss_function):
@@ -56,7 +56,7 @@ def validation_phase(NN_model,val_set_loader,loss_function):
     loss_val = 0
 
     for batch_id, (patch1, patch2, label) in enumerate(val_set_loader, 1):
-        if(config.Config.device.type == 'cuda'):
+        if(configuration.Config.device.type == 'cuda'):
             patch1, patch2, label = patch1.cuda(), patch2.cuda(), label.cuda()
         else:
             patch1, patch2, label = patch1,patch2,label
@@ -71,17 +71,17 @@ def validation_phase(NN_model,val_set_loader,loss_function):
 
     return loss_val/mini_batches
     
-def train(NN_model,train_set_loader,val_set_loader,loss_function,optimizer,config):
+def train(NN_model,train_set_loader,val_set_loader,loss_function,optimizer,configuration):
     wandb.watch(NN_model,loss_function,log='all',log_freq=50)
     
     num_seen_samples = 0
     mini_batches = 0
     loss_value = 0
 
-    for epoch in range(0,config.Config.train_number_epochs):
+    for epoch in range(0,configuration.Config.train_number_epochs):
         for batch_id, (patch1, patch2, label) in enumerate(train_set_loader, 1):
             NN_model.train()
-            if(config.Config.device.type == 'cuda'):
+            if(configuration.Config.device.type == 'cuda'):
                 patch1, patch2, label = patch1.cuda(), patch2.cuda(), label.cuda()
             else:
                 patch1, patch2, label = patch1,patch2,label
@@ -100,10 +100,10 @@ def train(NN_model,train_set_loader,val_set_loader,loss_function,optimizer,confi
             loss_value += float(loss)
 
             if (mini_batches % 200) == 0:
-                print("Training loss after %d batches"%(int(num_seen_samples/config.Config.train_batch_size)))
+                print("Training loss after %d batches"%(int(num_seen_samples/configuration.Config.train_batch_size)))
 
             #Plotting in wand b
-            if (mini_batches % config.Config.plot_frequency == 0):
+            if (mini_batches % configuration.Config.plot_frequency == 0):
                 val_loss = validation_phase(NN_model,val_set_loader,loss_function)
 
                 training_log(loss_value,mini_batches)
