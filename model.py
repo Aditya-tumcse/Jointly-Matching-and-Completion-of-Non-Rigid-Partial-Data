b86700f688a1bc00008840e17f0ac3556e0eb7ac
ge46tik <aditya.srinivas@tum.de>
2021-03-13 12:35:10 +0100

    No changes

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import train
 import wandb
 import config
 
-params = dict(config.Config.train_number_epochs,config.Config.train_batch_size,config.Config.learning_rate,config.Training_Data_Config.training_data_size,config.Config.contrastive_margin,config.Config.resnet_depth)
+params = dict(epochs=config.Config.train_number_epochs,batch_size=config.Config.train_batch_size,lr=config.Config.learning_rate,dataset_size=config.Training_Data_Config.training_data_size,loss_margin=config.Config.contrastive_margin,resnet_depth=config.Config.resnet_depth)
 
 if __name__ == '__main__':
     wandb.login()
