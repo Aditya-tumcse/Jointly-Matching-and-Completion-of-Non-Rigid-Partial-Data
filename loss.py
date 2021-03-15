e497733055deedb23bd861501240883965185a6d
ge46tik <aditya.srinivas@tum.de>
2021-03-15 21:47:57 +0100

    value of epsilon changed

--- loss.py
+++ loss.py
@@ -5,10 +5,10 @@ class ContrastiveLoss(nn.Module):
     def __init__(self,margin = 2.0):
         super(ContrastiveLoss,self).__init__()
         self.margin = margin
-        self.eps = 1e-6
+        self.eps = 1e-9
     
     def forward(self,distances,target,size_average = True):
-        losses = 0.5*((target.float32() * distances) + (1 - target).float32() * nn.functional.relu(self.margin - (distances + self.eps).sqrt()).pow(2))
+        losses = 0.5*((target.float() * distances) + (1 - target).float() * nn.functional.relu(self.margin - (distances + self.eps).sqrt()).pow(2))
         if size_average:
             return losses.mean()
         else:
