2d4a93bccf5ad5bc54dbf39493f5a2c6fcea5690
ge46tik <aditya.srinivas@tum.de>
2021-03-16 22:24:03 +0100

    Added triplet loss

--- loss.py
+++ loss.py
@@ -13,3 +13,14 @@ class ContrastiveLoss(nn.Module):
             return losses.mean()
         else:
             return losses.sum()
+
+class TripletLoss(nn.Module):
+    def __init__(self, margin):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, anchor, positive, negative, size_average=True):
+        distance_positive = (anchor - positive).pow(2).sum(1)  # .pow(.5)
+        distance_negative = (anchor - negative).pow(2).sum(1)  # .pow(.5)
+        losses = nn.functional.relu(distance_positive - distance_negative + self.margin)
+        return losses.mean() if size_average else losses.sum()
