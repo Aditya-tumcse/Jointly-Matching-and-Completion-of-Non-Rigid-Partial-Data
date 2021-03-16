23f70fcf14779f2b4b45c759fb514ce56cdcaf03
ge46tik <aditya.srinivas@tum.de>
2021-03-16 22:23:43 +0100

    changes in the values

--- configuration.py
+++ configuration.py
@@ -3,22 +3,22 @@ import torch
 class training_configuration():
     #training_set_dir = "/cluster/sorona/hyu/ptdcv/ballet/"
     training_set_dir = "/home/aditya/PycharmProjects/OpenCV-python/Project_2/patch_files/"
-    train_batch_size = 64
-    train_number_epochs = 100
+    train_batch_size = 2
+    train_number_epochs = 8
     learning_rate = 0.0001
     contrastive_margin = 1.0
     resnet_depth = 18
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     number_workers = 4
-    plot_frequency = 100
+    plot_frequency = 1
     
 class Training_Data_Config():
     number_keypoints = 400
     stride = 4
-    training_data_size = 50000
+    training_data_size = 2
 
 class Validation_Data_Config():
     number_keypoints = 416
     stride = 4
-    validation_data_size = 20000
+    validation_data_size = 2
     validation_set_dir = "/cluster/sorona/hyu/ptdcv/goalkeeper/"
